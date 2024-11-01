aaceab6a75bd6a8ac2f68cdd5cd8cbb0b4e52c4e
klaxay <laxaykaushik@gmail.com>
2024-11-01 15:30:40 +0530

    Added State Functionalities in Jokes App

--- jokes-app/src/components/Joke.js
+++ jokes-app/src/components/Joke.js
@@ -1,10 +1,20 @@
 import React from 'react'
 
 export default function Joke({question, punchline}){
+    const [isShown, setIsShown] = React.useState(false)
+    const [showVariable, setShowVariable]=React.useState('Show')
+    function toggleIsShown(){
+        setIsShown(prevShown => !prevShown)
+        setShowVariable(prevVariable=>{
+            if(prevVariable=='Show'){return 'Hide'}
+            else{return 'Show'}
+        })
+    }
     return (
         <div>
             <h3>{question}</h3>
-            <h5>{punchline}</h5>
+            {isShown && <h5>{punchline}</h5>}
+            <button onClick={toggleIsShown}>{showVariable} Punchline</button>
             <hr/>
         </div>
     )
